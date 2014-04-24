8b1abe922fc38e695f00b3afd210e39dad5fda1c
Noah <noahd@vt.edu>
2014-04-24 13:02:46 -0400

    added a timer

--- SpiTest.c
+++ SpiTest.c
@@ -1,34 +1,31 @@
 /*
-    5-10-07
-    Copyright Spark Fun Electronics© 2007
-    Nathan Seidle
-    nathan at sparkfun.com
-    
+    4-24-2014
+    N64 Controller Project
     ATmega328p
-	
-	Example Blink
-	Toggles all IO pins at 1Hz
-	I added a new useless comment
-	I also added a useless comment - Hunter
 */
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
 
 //Define functions
 //======================
-void delay_ms(uint16_t x); //General purpose delay
 void SPI_MasterInit(void);
 void SPI_MasterTransmit(char cData);
+void initTimer0(void);
 //======================
+
+//global var
+int alt = 0;
 
 int main (void)
 {
-    SPI_MasterInit();
+	initTimer0();
+    /*SPI_MasterInit();
 	while(1)
 	{
 		SPI_MasterTransmit(0x0F);
-		delay_ms(500);
-	}
+	}*/
+	while(1) {}//sit and loop while testing interrupt
 	
     return(0);
 }
@@ -51,15 +48,39 @@ void SPI_MasterTransmit(char cData)
 	while(!(SPSR & (1 << SPIF)))
 		;
 }
-//General short delays
-void delay_ms(uint16_t x)
+
+// this code sets up timer1 for a .25s interrupt @ 16Mhz Clock (mode 4)
+void initTimer0(void)
 {
-  uint8_t y, z;
-  for ( ; x > 0 ; x--){
-    for ( y = 0 ; y < 90 ; y++){
-      for ( z = 0 ; z < 6 ; z++){
-        asm volatile ("nop");
-      }
-    }
-  }
+    OCR1A = 0xF42;//3906 in hex = .25s
+
+    TCCR1B |= (1 << WGM12);
+    // Mode 4, CTC on OCR1A
+
+    TIMSK1 |= (1 << OCIE1A);
+    //Set interrupt on compare match
+
+    TCCR1B |= (1 << CS12) | (1 << CS10);
+    // set prescaler to 1024 and start the timer (64 us resolution)
+
+
+    sei();
+    // enable interrupts
+}
+
+ISR (TIMER1_COMPA_vect)
+{
+    // action to be done every .25s sec
+	//blink the LED again
+	if(alt == 0)
+	{
+		PORTC = 0xFF;//all port C high
+		alt = 1;
+	}
+	else if(alt == 1)
+	{
+		PORTC = 0x00;//all port c low
+		alt = 0;
+	}
+	
 }
